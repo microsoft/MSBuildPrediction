1004ccbbf02ffa6a5392d38024b13f8b8621b4f1
David Federman <dfederm@microsoft.com>
2019-06-12 10:12:40 -0700

    Add option to limit parallelism

---
+++ src/BuildPrediction/PredictionOptions.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Build.Prediction
+{
+    using System;
+
+    /// <summary>
+    /// Represents various options used during prediction to change behavior.
+    /// </summary>
+    public sealed class PredictionOptions
+    {
+        /// <summary>
+        /// Gets or sets the max degree of parallelism to use for prediction execution. Defaults to <see cref="Environment.ProcessorCount"/>.
+        /// </summary>
+        /// <remarks>
+        /// If the caller of <see cref="ProjectStaticPredictionExecutor"/> is parallelizing across projects, it's recommended to set this to 1 to avoid over-scheduling.
+        /// </remarks>
+        public int MaxDegreeOfParallelism { get; set; } = Environment.ProcessorCount;
+    }
+}
