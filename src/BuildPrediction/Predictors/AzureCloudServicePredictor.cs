2dc72d5d8a9c2360ec89e18a6efed6edb9d98a3c
David Federman <dfederm@microsoft.com>
2019-06-20 13:58:15 -0700

    Add AzureCloudServicePredictor

---
+++ src/BuildPrediction/Predictors/AzureCloudServicePredictor.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Build.Prediction.Predictors
+{
+    using System;
+    using Microsoft.Build.Evaluation;
+    using Microsoft.Build.Execution;
+
+    /// <summary>
+    /// Predicts inputs for Azure Cloud Service projects based on the service definition files (*.csdef) and service configuration files (*.cscfg).
+    /// </summary>
+    public class AzureCloudServicePredictor : IProjectPredictor
+    {
+        internal const string ServiceConfigurationItemName = "ServiceConfiguration";
+
+        internal const string ServiceDefinitionItemName = "ServiceDefinition";
+
+        /// <inheritdoc/>
+        public void PredictInputsAndOutputs(
+            Project project,
+            ProjectInstance projectInstance,
+            ProjectPredictionReporter predictionReporter)
+        {
+            // This predictor only applies to ccproj files
+            if (!projectInstance.FullPath.EndsWith(".ccproj", StringComparison.OrdinalIgnoreCase))
+            {
+                return;
+            }
+
+            foreach (ProjectItem item in project.GetItems(ServiceConfigurationItemName))
+            {
+                predictionReporter.ReportInputFile(item.EvaluatedInclude);
+            }
+
+            foreach (ProjectItem item in project.GetItems(ServiceDefinitionItemName))
+            {
+                predictionReporter.ReportInputFile(item.EvaluatedInclude);
+            }
+        }
+    }
+}
---
+++ src/BuildPredictionTests/Predictors/AzureCloudServicePredictorTests.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Build.Prediction.Tests.Predictors
+{
+    using Microsoft.Build.Construction;
+    using Microsoft.Build.Evaluation;
+    using Microsoft.Build.Prediction.Predictors;
+    using Xunit;
+
+    public class AzureCloudServicePredictorTests
+    {
+        [Fact]
+        public void FindItems()
+        {
+            Project project = CreateTestProject("project.ccproj");
+            var expectedInputFiles = new[]
+            {
+                new PredictedItem("ServiceDefinition.csdef", nameof(AzureCloudServicePredictor)),
+                new PredictedItem("ServiceConfiguration.Local.cscfg", nameof(AzureCloudServicePredictor)),
+                new PredictedItem("ServiceConfiguration.Prod.cscfg", nameof(AzureCloudServicePredictor)),
+            };
+            new AzureCloudServicePredictor()
+                .GetProjectPredictions(project)
+                .AssertPredictions(
+                    project,
+                    expectedInputFiles,
+                    null,
+                    null,
+                    null);
+        }
+
+        [Fact]
+        public void SkipOtherProjectTypes()
+        {
+            Project project = CreateTestProject("project.csproj");
+            new AzureCloudServicePredictor()
+                .GetProjectPredictions(project)
+                .AssertNoPredictions();
+        }
+
+        private static Project CreateTestProject(string fileName)
+        {
+            ProjectRootElement projectRootElement = ProjectRootElement.Create(fileName);
+            ProjectItemGroupElement itemGroup = projectRootElement.AddItemGroup();
+            itemGroup.AddItem(AzureCloudServicePredictor.ServiceDefinitionItemName, "ServiceDefinition.csdef");
+            itemGroup.AddItem(AzureCloudServicePredictor.ServiceConfigurationItemName, "ServiceConfiguration.Local.cscfg");
+            itemGroup.AddItem(AzureCloudServicePredictor.ServiceConfigurationItemName, "ServiceConfiguration.Prod.cscfg");
+
+            return TestHelpers.CreateProjectFromRootElement(projectRootElement);
+        }
+    }
+}
