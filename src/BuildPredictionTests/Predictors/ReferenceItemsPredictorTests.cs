ff2ef410e02346af27d2176e5a11d2b908309633
David Federman <dfederm@microsoft.com>
2019-07-02 16:32:33 -0700

    Support related files in ReferenceItemsPredictor

--- src/BuildPredictionTests/Predictors/ReferenceItemsPredictorTests.cs
+++ src/BuildPredictionTests/Predictors/ReferenceItemsPredictorTests.cs
@@ -16,20 +16,39 @@ namespace Microsoft.Build.Prediction.Tests.Predictors
         {
             ProjectRootElement projectRootElement = ProjectRootElement.Create(@"src\project.csproj");
 
+            // AllowedReferenceRelatedFileExtensions is usually set in Microsoft.Common.CurrentVersion.targets
+            const string allowedReferenceRelatedFileExtensions = @"
+      .pdb;
+      .xml;
+      .pri;
+      .dll.config;
+      .exe.config
+";
+            projectRootElement.AddProperty(ReferenceItemsPredictor.AllowedReferenceRelatedFileExtensionsPropertyName, allowedReferenceRelatedFileExtensions);
+
             // Reference using HintPath - Is an input
             projectRootElement
                 .AddItem(ReferenceItemsPredictor.ReferenceItemName, "Reference1")
-                .AddMetadata(ReferenceItemsPredictor.HintPathMetadata, @"..\packages\Package1\lib\new45\Reference1.dll");
+                .AddMetadata(ReferenceItemsPredictor.HintPathMetadata, @"..\packages\Package1\lib\net45\Reference1.dll");
 
             // Reference not using HintPath (usually uses <Name> though) - Is an input
             projectRootElement
-                .AddItem(ReferenceItemsPredictor.ReferenceItemName, @"..\packages\Package2\lib\new45\Reference2.dll")
+                .AddItem(ReferenceItemsPredictor.ReferenceItemName, @"..\packages\Package2\lib\net45\Reference2.dll")
                 .AddMetadata("Name", "Reference2");
 
             // Reference adjacent to the project (must exist) - Is an input
             projectRootElement.AddItem(ReferenceItemsPredictor.ReferenceItemName, @"CheckedInAssembly.dll");
             Directory.CreateDirectory("src");
             File.WriteAllText(@"src\CheckedInAssembly.dll", "SomeContent");
+
+            // Reference with related files (which must exist) - Is an input as well as related files
+            projectRootElement
+                .AddItem(ReferenceItemsPredictor.ReferenceItemName, @"..\packages\Package3\lib\net45\Reference3.dll")
+                .AddMetadata("Name", "Reference3");
+            Directory.CreateDirectory(@"packages\Package3\lib\net45");
+            File.WriteAllText(@"packages\Package3\lib\net45\Reference3.dll", "SomeContent");
+            File.WriteAllText(@"packages\Package3\lib\net45\Reference3.pdb", "SomeContent");
+            File.WriteAllText(@"packages\Package3\lib\net45\Reference3.xml", "SomeContent");
 
             // References from the platform or GAC - NOT inputs
             projectRootElement.AddItem(ReferenceItemsPredictor.ReferenceItemName, "System.Data");
@@ -39,8 +58,11 @@ namespace Microsoft.Build.Prediction.Tests.Predictors
 
             var expectedInputFiles = new[]
             {
-                new PredictedItem(@"packages\Package1\lib\new45\Reference1.dll", nameof(ReferenceItemsPredictor)),
-                new PredictedItem(@"packages\Package2\lib\new45\Reference2.dll", nameof(ReferenceItemsPredictor)),
+                new PredictedItem(@"packages\Package1\lib\net45\Reference1.dll", nameof(ReferenceItemsPredictor)),
+                new PredictedItem(@"packages\Package2\lib\net45\Reference2.dll", nameof(ReferenceItemsPredictor)),
+                new PredictedItem(@"packages\Package3\lib\net45\Reference3.dll", nameof(ReferenceItemsPredictor)),
+                new PredictedItem(@"packages\Package3\lib\net45\Reference3.pdb", nameof(ReferenceItemsPredictor)),
+                new PredictedItem(@"packages\Package3\lib\net45\Reference3.xml", nameof(ReferenceItemsPredictor)),
                 new PredictedItem(@"src\CheckedInAssembly.dll", nameof(ReferenceItemsPredictor)),
             };
             new ReferenceItemsPredictor()
