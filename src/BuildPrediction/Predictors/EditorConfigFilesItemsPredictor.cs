a465cda37d4e84d877b88f5c3ebb306aaf613005
David Federman <dfederm@microsoft.com>
2019-10-28 09:41:35 -0700

    Add predictor for editorconfig (#43)

---
+++ src/BuildPrediction/Predictors/EditorConfigFilesItemsPredictor.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Build.Prediction.Predictors
+{
+    using Microsoft.Build.Evaluation;
+    using Microsoft.Build.Execution;
+
+    /// <summary>
+    /// Finds Compile items, typically but not necessarily always from csproj files, as inputs.
+    /// </summary>
+    public class EditorConfigFilesItemsPredictor : IProjectPredictor
+    {
+        internal const string EditorConfigFilesItemName = "EditorConfigFiles";
+
+        /// <inheritdoc/>
+        public void PredictInputsAndOutputs(
+            Project project,
+            ProjectInstance projectInstance,
+            ProjectPredictionReporter predictionReporter)
+        {
+            foreach (ProjectItemInstance item in projectInstance.GetItems(EditorConfigFilesItemName))
+            {
+                predictionReporter.ReportInputFile(item.EvaluatedInclude);
+            }
+        }
+    }
+}
---
+++ src/BuildPredictionTests/Predictors/EditorConfigFilesItemsPredictorTests.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Build.Prediction.Tests.Predictors
+{
+    using Microsoft.Build.Construction;
+    using Microsoft.Build.Evaluation;
+    using Microsoft.Build.Prediction.Predictors;
+    using Xunit;
+
+    public class EditorConfigFilesItemsPredictorTests
+    {
+        [Fact]
+        public void FindItems()
+        {
+            ProjectRootElement projectRootElement = ProjectRootElement.Create();
+            projectRootElement.AddItem(EditorConfigFilesItemsPredictor.EditorConfigFilesItemName, ".editorconfig");
+
+            Project project = TestHelpers.CreateProjectFromRootElement(projectRootElement);
+
+            var expectedInputFiles = new[]
+            {
+                new PredictedItem(".editorconfig", nameof(EditorConfigFilesItemsPredictor)),
+            };
+            new EditorConfigFilesItemsPredictor()
+                .GetProjectPredictions(project)
+                .AssertPredictions(
+                    project,
+                    expectedInputFiles,
+                    null,
+                    null,
+                    null);
+        }
+    }
+}
