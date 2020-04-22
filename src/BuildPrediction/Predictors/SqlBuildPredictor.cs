f5550770c2d9b22ac8fd550cd470728c403363c1
David Federman <dfederm@microsoft.com>
2020-04-22 16:47:22 -0700

    Add SqlBuildPredictor

---
+++ src/BuildPrediction/Predictors/SqlBuildPredictor.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Build.Prediction.Predictors
+{
+    using System;
+    using Microsoft.Build.Execution;
+
+    /// <summary>
+    /// Makes predictions for the SqlBuild target for sqlproj projects.
+    /// </summary>
+    public sealed class SqlBuildPredictor : IProjectPredictor
+    {
+        internal const string BuildItemName = "Build";
+        internal const string PostDeployItemName = "PostDeploy";
+        internal const string PreDeployItemName = "PreDeploy";
+
+        /// <inheritdoc/>
+        public void PredictInputsAndOutputs(
+            ProjectInstance projectInstance,
+            ProjectPredictionReporter predictionReporter)
+        {
+            // This predictor only applies to sqlproj files
+            if (!projectInstance.FullPath.EndsWith(".sqlproj", StringComparison.OrdinalIgnoreCase))
+            {
+                return;
+            }
+
+            foreach (ProjectItemInstance item in projectInstance.GetItems(BuildItemName))
+            {
+                predictionReporter.ReportInputFile(item.EvaluatedInclude);
+            }
+
+            foreach (ProjectItemInstance item in projectInstance.GetItems(PostDeployItemName))
+            {
+                predictionReporter.ReportInputFile(item.EvaluatedInclude);
+            }
+
+            foreach (ProjectItemInstance item in projectInstance.GetItems(PreDeployItemName))
+            {
+                predictionReporter.ReportInputFile(item.EvaluatedInclude);
+            }
+        }
+    }
+}
---
+++ src/BuildPredictionTests/Predictors/SqlBuildPredictorTests.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Build.Prediction.Tests.Predictors
+{
+    using Microsoft.Build.Construction;
+    using Microsoft.Build.Execution;
+    using Microsoft.Build.Prediction.Predictors;
+    using Xunit;
+
+    public class SqlBuildPredictorTests
+    {
+        [Fact]
+        public void FindItems()
+        {
+            ProjectRootElement projectRootElement = ProjectRootElement.Create("project.sqlproj");
+            ProjectItemGroupElement itemGroup = projectRootElement.AddItemGroup();
+            itemGroup.AddItem(SqlBuildPredictor.BuildItemName, "Build1.sql");
+            itemGroup.AddItem(SqlBuildPredictor.BuildItemName, "Build2.sql");
+            itemGroup.AddItem(SqlBuildPredictor.BuildItemName, "Build3.sql");
+            itemGroup.AddItem(SqlBuildPredictor.PreDeployItemName, "PreDeploy1.sql");
+            itemGroup.AddItem(SqlBuildPredictor.PreDeployItemName, "PreDeploy2.sql");
+            itemGroup.AddItem(SqlBuildPredictor.PreDeployItemName, "PreDeploy3.sql");
+            itemGroup.AddItem(SqlBuildPredictor.PostDeployItemName, "PostDeploy1.sql");
+            itemGroup.AddItem(SqlBuildPredictor.PostDeployItemName, "PostDeploy2.sql");
+            itemGroup.AddItem(SqlBuildPredictor.PostDeployItemName, "PostDeploy3.sql");
+            ProjectInstance projectInstance = TestHelpers.CreateProjectInstanceFromRootElement(projectRootElement);
+
+            var expectedInputFiles = new[]
+            {
+                new PredictedItem("Build1.sql", nameof(SqlBuildPredictor)),
+                new PredictedItem("Build2.sql", nameof(SqlBuildPredictor)),
+                new PredictedItem("Build3.sql", nameof(SqlBuildPredictor)),
+                new PredictedItem("PreDeploy1.sql", nameof(SqlBuildPredictor)),
+                new PredictedItem("PreDeploy2.sql", nameof(SqlBuildPredictor)),
+                new PredictedItem("PreDeploy3.sql", nameof(SqlBuildPredictor)),
+                new PredictedItem("PostDeploy1.sql", nameof(SqlBuildPredictor)),
+                new PredictedItem("PostDeploy2.sql", nameof(SqlBuildPredictor)),
+                new PredictedItem("PostDeploy3.sql", nameof(SqlBuildPredictor)),
+            };
+
+            new SqlBuildPredictor()
+                .GetProjectPredictions(projectInstance)
+                .AssertPredictions(
+                    projectInstance,
+                    expectedInputFiles,
+                    null,
+                    null,
+                    null);
+        }
+
+        [Fact]
+        public void SkipOtherProjectTypes()
+        {
+            ProjectRootElement projectRootElement = ProjectRootElement.Create("project.csproj");
+            ProjectItemGroupElement itemGroup = projectRootElement.AddItemGroup();
+            itemGroup.AddItem(SqlBuildPredictor.BuildItemName, "Build1.sql");
+            itemGroup.AddItem(SqlBuildPredictor.BuildItemName, "Build2.sql");
+            itemGroup.AddItem(SqlBuildPredictor.BuildItemName, "Build3.sql");
+            itemGroup.AddItem(SqlBuildPredictor.PreDeployItemName, "PreDeploy1.sql");
+            itemGroup.AddItem(SqlBuildPredictor.PreDeployItemName, "PreDeploy2.sql");
+            itemGroup.AddItem(SqlBuildPredictor.PreDeployItemName, "PreDeploy3.sql");
+            itemGroup.AddItem(SqlBuildPredictor.PostDeployItemName, "PostDeploy1.sql");
+            itemGroup.AddItem(SqlBuildPredictor.PostDeployItemName, "PostDeploy2.sql");
+            itemGroup.AddItem(SqlBuildPredictor.PostDeployItemName, "PostDeploy3.sql");
+            ProjectInstance projectInstance = TestHelpers.CreateProjectInstanceFromRootElement(projectRootElement);
+
+            new SqlBuildPredictor()
+                .GetProjectPredictions(projectInstance)
+                .AssertNoPredictions();
+        }
+    }
+}
