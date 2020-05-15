082663b383cbb23bcf6298c2a7f2af546feee72a
David Federman <dfederm@microsoft.com>
2020-05-15 09:17:58 -0700

    Add AnalyzerItemsPredictor

---
+++ src/BuildPrediction/Predictors/AnalyzerItemsPredictor.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Build.Prediction.Predictors
+{
+    using Microsoft.Build.Execution;
+
+    /// <summary>
+    /// Finds Analyzer items as inputs, used by the Roslyn compiler.
+    /// </summary>
+    public sealed class AnalyzerItemsPredictor : IProjectPredictor
+    {
+        internal const string AnalyzerItemName = "Analyzer";
+
+        /// <inheritdoc/>
+        public void PredictInputsAndOutputs(
+            ProjectInstance projectInstance,
+            ProjectPredictionReporter predictionReporter)
+        {
+            foreach (ProjectItemInstance item in projectInstance.GetItems(AnalyzerItemName))
+            {
+                predictionReporter.ReportInputFile(item.EvaluatedInclude);
+            }
+        }
+    }
+}
---
+++ src/BuildPredictionTests/Predictors/AnalyzerItemsPredictorTests.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Build.Prediction.Tests.Predictors
+{
+    using Microsoft.Build.Construction;
+    using Microsoft.Build.Execution;
+    using Microsoft.Build.Prediction.Predictors;
+    using Xunit;
+
+    public class AnalyzerItemsPredictorTests
+    {
+        [Fact]
+        public void FindItems()
+        {
+            ProjectRootElement projectRootElement = ProjectRootElement.Create();
+            ProjectItemGroupElement itemGroup = projectRootElement.AddItemGroup();
+            itemGroup.AddItem(AnalyzerItemsPredictor.AnalyzerItemName, "Foo.dll");
+            itemGroup.AddItem(AnalyzerItemsPredictor.AnalyzerItemName, "Bar.dll");
+            itemGroup.AddItem(AnalyzerItemsPredictor.AnalyzerItemName, "Baz.dll");
+            ProjectInstance projectInstance = TestHelpers.CreateProjectInstanceFromRootElement(projectRootElement);
+
+            var expectedInputFiles = new[]
+            {
+                new PredictedItem("Foo.dll", nameof(AnalyzerItemsPredictor)),
+                new PredictedItem("Bar.dll", nameof(AnalyzerItemsPredictor)),
+                new PredictedItem("Baz.dll", nameof(AnalyzerItemsPredictor)),
+            };
+
+            new AnalyzerItemsPredictor()
+                .GetProjectPredictions(projectInstance)
+                .AssertPredictions(
+                    projectInstance,
+                    expectedInputFiles,
+                    null,
+                    null,
+                    null);
+        }
+    }
+}
