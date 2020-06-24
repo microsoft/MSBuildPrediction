e9890a0bde9cdafc16e0eccc4302fd4eb8f87733
David Federman <dfederm@microsoft.com>
2020-06-24 12:15:30 -0700

    Add LinkItemsPredictor (#80)

---
+++ src/BuildPrediction/Predictors/LinkItemsPredictor.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Build.Prediction.Predictors
+{
+    using System;
+    using Microsoft.Build.Execution;
+
+    /// <summary>
+    /// Finds ClInclude items, typically header files used during compilation.
+    /// </summary>
+    public sealed class LinkItemsPredictor : IProjectPredictor
+    {
+        internal const string LinkItemName = "Link";
+
+        /// <inheritdoc/>
+        public void PredictInputsAndOutputs(
+            ProjectInstance projectInstance,
+            ProjectPredictionReporter predictionReporter)
+        {
+            // This predictor only applies to vcxproj files
+            if (!projectInstance.FullPath.EndsWith(".vcxproj", StringComparison.OrdinalIgnoreCase))
+            {
+                return;
+            }
+
+            foreach (ProjectItemInstance item in projectInstance.GetItems(LinkItemName))
+            {
+                predictionReporter.ReportInputFile(item.EvaluatedInclude);
+            }
+        }
+    }
+}
